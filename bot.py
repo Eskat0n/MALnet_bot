0f8bb04c84959238df31a0aacb12c06cdd951637
eskat0n <muyou.prj@gmail.com>
2016-04-07 03:29:06 +0500

    first version on daemonization wrapper for bot

--- bot.py
+++ bot.py
@@ -7,63 +7,70 @@ from emoji import emojize
 import mal
 from config import read_bot_config
 
-bot_config = read_bot_config()
-bot = telebot.TeleBot(bot_config['Token'])
+
+class MALnetBot(telebot.TeleBot):
+    def __init__(self):
+        super().__init__(read_bot_config()['Token'])
+
+        self.add_message_handler(self.send_help,
+                                 commands=['start', 'help'])
+        self.add_message_handler(self.chat_send_search_entries,
+                                 func=lambda m: True)
+
+        self.add_inline_handler(self.inline_send_search_entries,
+                                func=lambda q: True)
+
+    def send_help(self, message):
+        pass
+
+    def chat_send_search_entries(self, message):
+        chat_id = message.chat.id
+        entries = self.search_entries(message.text)
+
+        if len(entries) == 0:
+            self.send_message(chat_id,
+                              text=emojize("Nothing found :cry:", use_aliases=True),
+                              disable_notification=True)
+        else:
+            self.send_entry(chat_id, entries[0])
+
+    def inline_send_search_entries(self, query):
+        entries = self.search_entries(query.query)
+        results = [InlineQueryResultArticle(entry.id, entry.title, entry.to_markdown,
+                                            parse_mode='Markdown',
+                                            disable_web_page_preview=True,
+                                            thumb_url=entry.image,
+                                            description=entry.score_info)
+                   for entry in entries[0:5]]
+
+        self.answer_inline_query(query.id, results)
+
+    def send_entry(self, chat_id, entry):
+        filename = '{}.jpg'.format(entry.id)
+        urlretrieve(entry.image, filename)
+        image = open(filename, 'rb')
+
+        self.send_photo(chat_id,
+                        photo=image,
+                        disable_notification=True)
+        self.send_message(chat_id,
+                          text=entry.to_markdown,
+                          parse_mode='Markdown',
+                          disable_web_page_preview=True,
+                          disable_notification=True)
+
+    def run(self):
+        self.polling()
+
+    @staticmethod
+    def search_entries(text):
+        words = text.split(' ')
+        if words[0] in ['m', 'manga']:
+            return mal.search_manga(' '.join(words[1:]))
+        else:
+            return mal.search_anime(text)
 
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    pass
-
-
-@bot.message_handler(func=lambda m: True)
-def chat_search_entries(message):
-    chat_id = message.chat.id
-
-    entries = search_entries(message.text)
-
-    if len(entries) == 0:
-        bot.send_message(chat_id,
-                         text=emojize("Nothing found :cry:", use_aliases=True),
-                         disable_notification=True)
-    else:
-        send_entry(chat_id, entries[0])
-
-
-@bot.inline_handler(func=lambda q: True)
-def inline_search_entries(query):
-    entries = search_entries(query.query)
-    results = [InlineQueryResultArticle(entry.id, entry.title, entry.to_markdown,
-                                        parse_mode='Markdown',
-                                        disable_web_page_preview=True,
-                                        thumb_url=entry.image,
-                                        description=entry.score_info)
-               for entry in entries[0:5]]
-
-    bot.answer_inline_query(query.id, results)
-
-
-def search_entries(text):
-    words = text.split(' ')
-    if words[0] in ['m', 'manga']:
-        return mal.search_manga(' '.join(words[1:]))
-    else:
-        return mal.search_anime(text)
-
-
-def send_entry(chat_id, entry):
-    filename = '{}.jpg'.format(entry.id)
-    urlretrieve(entry.image, filename)
-    image = open(filename, 'rb')
-
-    bot.send_photo(chat_id,
-                   photo=image,
-                   disable_notification=True)
-    bot.send_message(chat_id,
-                     text=entry.to_markdown,
-                     parse_mode='Markdown',
-                     disable_web_page_preview=True,
-                     disable_notification=True)
-
-
-bot.polling()
+if __name__ == '__main__':
+    bot = MALnetBot()
+    bot.run()
---
+++ daemon.py
@@ -0,0 +1,7 @@
+from daemonize import Daemonize
+from bot import MALnetBot
+
+bot = MALnetBot()
+
+daemon = Daemonize(app="MALnet_bot", pid="/bot_daemon.pid", action=bot.run)
+daemon.start()
