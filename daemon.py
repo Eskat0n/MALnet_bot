ab95389d9c80c81476fa01e75f55d2dcb37c80d0
eskat0n <muyou.prj@gmail.com>
2016-04-07 03:40:12 +0500

    fixed path to pid file

--- daemon.py
+++ daemon.py
@@ -3,5 +3,5 @@ from bot import MALnetBot
 
 bot = MALnetBot()
 
-daemon = Daemonize(app="MALnet_bot", pid="/bot_daemon.pid", action=bot.run)
+daemon = Daemonize(app="MALnet_bot", pid="bot_daemon.pid", action=bot.run)
 daemon.start()
